c301d50e1d1e9cb624bce85da81e75f86ff92a12
AidanJP <aidanpendlebury@gmail.com>
2020-07-15 18:34:45 +0100

    Initial Commit

---
+++ Shared/ActivityCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityCountdownApp.swift
+//  Shared
+//
+//  Created by Aidan Pendlebury on 15/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct ActivityCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
