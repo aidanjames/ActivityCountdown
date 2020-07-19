26bc853329882ff3af6abf1e5f143f0d9c9b1eb5
AidanJP <aidanpendlebury@gmail.com>
2020-07-19 06:47:05 +0100

    Noticed that the latest data still wasn't being pulled through so I added an ugly async after with a 1 second delay upon returning to the foreground and this appears to work.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -23,8 +23,10 @@ struct ContentView: View {
         }
         .padding()
         .frame(width: 400, height: 300)
-        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
-            healthData.executeActivitySummaryQuery()
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                healthData.executeActivitySummaryQuery()
+            }
         }
     }
 }
